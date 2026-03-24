b2fea22b152f03817ed5e66f87031bfdafcf1df1
Amine EL ZOUIRI <97691793+amineelzouiri@users.noreply.github.com>
2026-03-24 10:30:46 +0100

    Update calculer_probabilites_admission.py

--- fonctions/calculer_probabilites_admission.py
+++ fonctions/calculer_probabilites_admission.py
@@ -2,17 +2,7 @@
 #Auteurs : Amine, Jarod, Mouheb
 
 def calculer_probabilites_admission(df_filtered):
-    """
-    Calcule les probabilités moyennes d'admission selon le type de baccalauréat
-    (général, technologique, professionnel) pour les formations filtrées.
-
-    Paramètre :
-        dataframe (DataFrame) : DataFrame filtré selon les critères de l'utilisateur
-
-    Retourne :
-        dict : {'general': float, 'tech': float, 'pro': float}
-               Retourne des zéros si le DataFrame est vide.
-    """
+    
     if df_filtered.empty:
         return {"general": 0, "pro": 0, "tech": 0}
 
