0221853eccf30d6521ad12077d6cf5f0d0fe12a1
Amine EL ZOUIRI <97691793+amineelzouiri@users.noreply.github.com>
2026-03-24 10:32:11 +0100

    Update mettre_a_jour_listes.py

--- fonctions/mettre_a_jour_listes.py
+++ fonctions/mettre_a_jour_listes.py
@@ -4,20 +4,6 @@
 from fonctions.filtrer_donnees import filtrer_donnees
 
 def mettre_a_jour_listes(param: dict):
-    """
-    Met à jour dynamiquement les listes déroulantes du simulateur en fonction
-    des filtres déjà sélectionnés par l'utilisateur (requête AJAX).
-
-    Paramètres :
-        lieu (str)       : région sélectionnée
-        choix_dep (str)  : filière agrégée sélectionnée
-        choix_ville (str): commune sélectionnée
-        choix_fil (str)  : filière sélectionnée
-        dataframe (DataFrame) : DataFrame Parcoursup complet
-
-    Retourne :
-        dict : {'villes': list, 'filieres': list, 'postbacs': list}
-    """
 
     df_temp_filtered = filtrer_donnees(param)
 
