f540c5f00aaf6ce8a5027abff4d29a1e30a84217
Amine EL ZOUIRI <97691793+amineelzouiri@users.noreply.github.com>
2026-03-24 10:31:40 +0100

    Update definir_listes.py

--- fonctions/definir_listes.py
+++ fonctions/definir_listes.py
@@ -2,16 +2,6 @@
 #Auteurs : Amine, Jarod, Mouheb
 
 def definir_listes(dataframe):
-    """
-    Génère les 5 listes de valeurs uniques triées utilisées pour
-    alimenter les menus déroulants du simulateur Parcoursup.
-
-    Paramètre :
-        dataframe (DataFrame) : DataFrame Parcoursup complet
-
-    Retourne :
-        tuple : (filières agrégées, régions, communes, filières, filières détaillées)
-    """
     listes = []
     for entete in ["Filière de formation très agrégée", "Région de l’établissement", "Commune de l’établissement",
                    "Filière de formation", "Filière de formation détaillée"]:
