497aef23d388cf6fc79730df863c1acf6d8e481c
Amine EL ZOUIRI <97691793+amineelzouiri@users.noreply.github.com>
2026-03-24 10:31:20 +0100

    Update construire_resultats.py

--- fonctions/construire_resultats.py
+++ fonctions/construire_resultats.py
@@ -8,17 +8,7 @@ from fonctions.filtrer_donnees import filtrer_donnees
 
 
 def construire_resultats(parametres: dict):
-    """
-    Agrège toutes les données nécessaires à l'affichage du simulateur Parcoursup :
-    probabilités d'admission, répartition des mentions et listes des filtres disponibles.
-
-    Paramètres :
-        lieu, choix_dep, choix_ville, choix_fil, choix_postbac, choix_etablissement (str) : filtres
-        dataframe (DataFrame) : DataFrame Parcoursup complet
-
-    Retourne :
-        dict : résultats complets pour affichage dans le template index.html
-    """
+    
     df_filtered = filtrer_donnees(parametres)
     liste_filiere_agregee, liste_region, liste_etablissement, liste_de_filiere, liste_filiere_postbac = \
         definir_listes(parametres['dataframe'])
